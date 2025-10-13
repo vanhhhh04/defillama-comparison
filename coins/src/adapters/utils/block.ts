4fd1de41c04b4517ac510926d3c15d769b9ef451
anh_cao <anh_cao>
2025-10-13 16:25:41 +0700

    base version

--- coins/src/adapters/utils/block.ts
+++ coins/src/adapters/utils/block.ts
@@ -6,7 +6,6 @@ export default async function getBlock(
   chain: any,
   timestamp: number,
 ): Promise<number | undefined> {
-  console.log("Getting block for", chain, timestamp);
   if (timestamp == 0) return;
   if (chain == "era") {
     try {
